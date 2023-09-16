9ad9866421ce105066b9d80d660288e856f39674
@lahiruchathuranga19981214 <lahiruchathuranga19981214@gmail.com>
2023-09-16 18:18:19 +0530

    CHG | Cors policy updated

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: 'http://localhost:3000', 
+        origin: 'https://witty-grass-0a3dca310.3.azurestaticapps.net/', 
         credentials: true,
     })
 );
